56245637eb5f1836cf31712a387eb3942d80fe4d
Sebastian Godinez <sebastian.godinez@hotmail.com>
2018-12-02 02:01:35 -0300

    empiezo a integrar cliente

--- src/Pregunta_2/Cliente.java
+++ src/Pregunta_2/Cliente.java
@@ -1,16 +1,12 @@
-package Pregunta_2;
-
-/**
- * Cliente
- */
-public class Cliente extends Main implements Runnable{
-
-    private String nombre;
+public class Cliente implements Runnable{
+    volatile String nombre;
     private Thread t;
     public Cliente(String nombre){
         this.nombre = nombre;
         t = new Thread(this, nombre);
         t.start();
     }
-    
+    public void run(){
+        System.out.println("Hola soy: " + nombre);
+    }
 }
--- src/Pregunta_2/Main.java
+++ src/Pregunta_2/Main.java
@@ -1,13 +1,8 @@
-package Pregunta_2;
-
-/**
- * Main
- */
 public class Main {
+    
     public static void main(String[] args) {
         String nombre_cliente = "";
-
-        while (!nombre_cliente.equals("salir")) {
+        while (!nombre_cliente.equals("Salir")) {
             System.out.println("Ingrese Nombre de Cliente:"
             + "\n(para salir ingrese \"Salir\")");
             try {
@@ -16,7 +11,7 @@ public class Main {
                 System.out.println("Ingrese una opcion valida!");
                 nombre_cliente = "";
             }
-            System.out.println(nombre_cliente);
+            Cliente test = new Cliente(nombre_cliente);
         }
     }
 }
